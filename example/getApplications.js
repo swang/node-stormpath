d1d9876ea3009233f526c93a36b23337847b3749
Shuan Wang <shuan@metamarkets.com>
2013-06-18 14:46:39 -0700

    Added examples from README

---
+++ example/getApplications.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var c, options, Tenant = require('./lib/stormpath').Tenant
+
+options = {
+  apiId: "<API ID>"
+  , apiSecret: "<API Secret>"
+}
+
+c = new Client(options)
+c.getCurrentTenant(function(err, ten) {
+  if (err) console.log(err)
+  else { 
+    console.log("Tenant id is: " + ten.id)
+    ten.getApplications(function(err, apps) {
+      if (err) console.log(err)
+      else {
+        apps.forEach(function(app) {
+          console.log(app.id, "::", app.getData('name'), "::", app.getData('href'), "::", app.getData('status'))
+        })
+      }
+    })
+  }
+})
---
+++ example/getCurrentTenant.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var c, options, Tenant = require('./lib/stormpath').Tenant
+
+options = {
+  apiId: "<API ID>"
+  , apiSecret: "<API Secret>"
+}
+
+c = new Client(options)
+c.getCurrentTenant(function(err, ten) {
+  if (err) console.log(err)
+  else { console.log("Tenant id is: " + ten.id) }
+})
